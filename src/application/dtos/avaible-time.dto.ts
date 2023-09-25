5e2a7e000e73e14414a537a274b2d802702b5982
Victor Hugo <victormotabatista@gmail.com>
2023-09-25 12:33:04 -0300

    dtos

---
+++ src/application/dtos/avaible-time.dto.ts
@@ -0,0 +1,10 @@
+export class AvailableTimeDto {
+  id: string;
+  week_day: string;
+  time_start: Date;
+  time_end: Date;
+  active: boolean;
+  is_deleted: boolean;
+  created_at: Date;
+  updated_at: Date;
+}
