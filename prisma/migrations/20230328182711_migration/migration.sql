c15bacc50c80d573c4804445851c2d52d0ca4f26
Victor Hugo <victormotabatista@gmail.com>
2023-09-29 23:29:38 -0300

    remove prisma

--- prisma/migrations/20230328182711_migration/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Adress" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "zipCode" TEXT NOT NULL,
-    "address" TEXT NOT NULL,
-    "city" TEXT NOT NULL,
-    "state" TEXT NOT NULL
-);
