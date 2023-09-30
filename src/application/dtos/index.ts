659870dfc07d36de613eb9947e558da9e1852d62
Victor Hugo <victormotabatista@gmail.com>
2023-09-29 23:08:34 -0300

    chore: updating index

--- src/application/dtos/index.ts
+++ src/application/dtos/index.ts
@@ -1,4 +1,2 @@
-export * from './create-address-body.dto';
-export * from './update-address-body.dto';
 export * from './user.dto';
 export * from './avaible-time.dto';
--- src/infra/http/view-models/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './list-address.view-model';
-export * from './address.view-model';
