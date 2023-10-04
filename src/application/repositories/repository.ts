601d2a18a610517e68a3827e27087c5c1c85806e
Victor Hugo <victormotabatista@gmail.com>
2023-10-04 18:03:03 -0300

    update repository

--- src/application/repositories/repository.ts
+++ src/application/repositories/repository.ts
@@ -3,15 +3,7 @@ export abstract class Repository<TEntity> {
 
   abstract sendDocument(data: TEntity): Promise<void>;
 
-  abstract update(code: string, data: any): Promise<void>;
-
   abstract update(username: string, data: any): Promise<void>;
 
-  abstract findByCode(code: string): Promise<TEntity>;
-
   abstract findByUsername(username: string): Promise<TEntity>;
-
-  abstract getAll(): Promise<TEntity[]>;
-
-  abstract delete(code: string): Promise<void>;
 }
