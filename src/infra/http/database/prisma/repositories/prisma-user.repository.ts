7848af97b2c7930ae97de40b378445558ad814e8
Victor Hugo <victormotabatista@gmail.com>
2023-10-04 18:02:36 -0300

    prisma

---
+++ src/infra/http/database/prisma/repositories/prisma-user.repository.ts
@@ -0,0 +1,22 @@
+import { UserRepository } from "@application/repositories";
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../prisma.service";
+import { User } from "@domain/entities";
+
+@Injectable()
+export class PrismaUserRepository implements UserRepository {
+    constructor(private prismaService: PrismaService) {}
+    
+    create(data: User): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    sendDocument(data: User): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    update(username: string, data: any): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    findByUsername(username: string): Promise<User> {
+        throw new Error("Method not implemented.");
+    }
+};
