76b398e78087037314ea7cef3e15614f71d0cfdf
Victor Hugo <victormotabatista@gmail.com>
2023-10-04 19:17:37 -0300

    create user controller

---
+++ src/infra/http/controllers/user.controllers.ts
@@ -0,0 +1,53 @@
+import {
+  CreateUserUseCase,
+  GetByUsernameUseCase,
+  UpdateUserUseCase,
+} from '@application/use-cases';
+import { Body, Controller, Get, Param, Post, Put, Req } from '@nestjs/common';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { UserControllerDto } from '../dtos';
+import { UserUpdateControllerDto } from '../dtos/user-update.controller.dto';
+import { UserViewModel } from '../view-models';
+
+@ApiTags('User')
+@Controller('user')
+export class UserController {
+  constructor(
+    private _createUserUseCase: CreateUserUseCase,
+    private _getByUsernameUseCase: GetByUsernameUseCase,
+    private _updateUserUseCase: UpdateUserUseCase,
+  ) {}
+
+  @ApiOperation({ summary: 'Create a new user' })
+  @ApiResponse({ status: 200 })
+  @Post()
+  async create(@Body() body: UserControllerDto) {
+    const { UserOutput } = await this._createUserUseCase.execute({
+      ...body,
+    });
+    return { UserOutput };
+  }
+
+  @ApiOperation({ summary: 'Get user by username' })
+  @ApiParam({ name: 'username', required: true })
+  @Get(':username')
+  async findByUsername(@Param() params) {
+    const { data } = await this._getByUsernameUseCase.execute({
+      username: params.username,
+    });
+
+    return { data: UserViewModel.toHttp(data) };
+  }
+
+  @ApiOperation({ summary: 'Updated a user' })
+  @ApiParam({ name: 'username', required: true })
+  @Put(':username')
+  async update(@Param() params, @Body() body: UserUpdateControllerDto) {
+    const { UserOutput } = await this._updateUserUseCase.execute(
+      params.username,
+      body,
+    );
+
+    return { UserOutput };
+  }
+}
