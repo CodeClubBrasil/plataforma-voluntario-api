5166c7fea5ce9448c19072e5b991c1b81a9b51de
Victor Hugo <victormotabatista@gmail.com>
2023-10-04 19:18:39 -0300

    feat(infra): creating a in memory repository

--- src/infra/http/database/in-memory/repositories/in-memory.repository.ts
+++ src/infra/http/database/in-memory/repositories/in-memory.repository.ts
@@ -1,44 +1,44 @@
-import { UserRepository } from "@application/repositories";
-import { User } from "@domain/entities";
-import { State } from "@domain/enums";
-import { UserUpdateControllerDto } from "@infra/http/dtos/user-update.controller.dto";
-import { Injectable } from "@nestjs/common";
+import { UserRepository } from '@application/repositories';
+import { User } from '@domain/entities';
+import { State } from '@domain/enums';
+import { UserUpdateControllerDto } from '@infra/http/dtos/user-update.controller.dto';
+import { Injectable } from '@nestjs/common';
 
-Injectable()
+Injectable();
 export class InMemoryUserRepository implements UserRepository {
-    private user: User[];
-    constructor() {
-      this.user = [];
-    }
+  private user: User[];
+  constructor() {
+    this.user = [];
+  }
 
   async create(data: User): Promise<void> {
-      this.user.push(data);
+    this.user.push(data);
   }
-  
+
   async sendDocument(data: User): Promise<void> {
-      throw new Error("Method not implemented.");
+    throw new Error('Method not implemented.');
   }
-  
+
   async update(username: string, data: UserUpdateControllerDto): Promise<void> {
     const userFindByUsername = this.user.find(
-        (user) => user.email === username,
-      );
+      (user) => user.email === username,
+    );
 
-      userFindByUsername.lastName = data.last_name;
-      userFindByUsername.password = data.password;
-      userFindByUsername.city = data.city;
-      userFindByUsername.name = data.name;
-      userFindByUsername.state = data.state as State;
-      userFindByUsername.neighborhood = data.neighborhood;
-      userFindByUsername.telephone = data.telephone;
-      //userFindByUsername.email = data.email;
+    userFindByUsername.lastName = data.last_name;
+    userFindByUsername.password = data.password;
+    userFindByUsername.city = data.city;
+    userFindByUsername.name = data.name;
+    userFindByUsername.state = data.state as State;
+    userFindByUsername.neighborhood = data.neighborhood;
+    userFindByUsername.telephone = data.telephone;
+    //userFindByUsername.email = data.email;
   }
-  
+
   async findByUsername(username: string): Promise<User> {
     const userFindByUsername = this.user.find(
-        (user) => user.email === username,
-      );
+      (user) => user.email === username,
+    );
 
-      return userFindByUsername;
+    return userFindByUsername;
   }
 }
