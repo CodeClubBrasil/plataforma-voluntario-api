17b0dc20806cdf082ae4f24f6e80a85053d1d8ea
Victor Hugo <victormotabatista@gmail.com>
2023-10-05 23:32:04 -0300

    update user controller dto

--- src/infra/http/dtos/user-controller.dto.ts
+++ src/infra/http/dtos/user-controller.dto.ts
@@ -1,34 +1,49 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { AvailableTimeControllerDto } from './avaible-time-contoller.dto';
+import { State } from '@domain/enums';
 
 export class UserControllerDto {
   id: string;
+  
   @ApiProperty()
   name: string;
+  
   @ApiProperty()
   last_name: string;
+  
   @ApiProperty()
   telephone: string[];
+  
   @ApiProperty()
   email: string;
+  
   @ApiProperty()
   password: string;
+  
   @ApiProperty()
   city: string;
-  @ApiProperty()
+  
+  @ApiProperty({ enum: State })
   state: string;
+  
   @ApiProperty()
   neighborhood: string;
+  
   @ApiProperty()
   know_ledges: string[];
+  
   @ApiProperty({ type: [AvailableTimeControllerDto] })
   available_time: AvailableTimeControllerDto[] | null;
+  
   @ApiProperty()
   active: boolean;
+  
   @ApiProperty()
   is_deleted: boolean;
+  
   @ApiProperty()
   created_at: Date;
+  
   @ApiProperty()
   updated_at: Date;
 }
