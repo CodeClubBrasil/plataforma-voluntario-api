8e4761612c4ecd03adfca0a00413afa3bd321dd2
Victor Hugo <victormotabatista@gmail.com>
2023-11-28 09:30:28 -0300

    fix: prisma repository

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -2,14 +2,13 @@ import { UserRepository } from '@application/repositories';
 import { Module } from '@nestjs/common';
 import { PrismaUserRepository } from './prisma/repositories/prisma-user.repository';
 import { PrismaService } from './prisma/prisma.service';
-import { InMemoryUserRepository } from './in-memory/repositories/in-memory.repository';
 
 @Module({
   providers: [
     PrismaService,
     {
       provide: UserRepository,
-      useClass: PrismaService,
+      useClass: PrismaUserRepository,
     },
   ],
   exports: [UserRepository],
